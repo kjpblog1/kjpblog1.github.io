87ebb4186ad53db82ae9930ddd072b9afb735d17
kjpblog1 <akhilgupta.kgp@outlook.com>
2015-08-15 23:00:21 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php ob_start();
+	session_start();
+	include 'sign.php';
+	if (isset($_SESSION['CurrentUser'])) {
+		header("Location:loggedin.php");
+	}
+?>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="sheet1.css">
+<link rel="icon" type="image/gif" href="myicon.png" sizes="16*16">
+<title>Blog-Home</title>
+</head>
+<body bgcolor="rgba(132,132,132,0.2)" width = "960px">
+<div id="container">
+	<div id="top">
+		<div id="kgplogo"></div>
+		<div id="toptext">IIT KHARAGUR BLOG</div>
+		<div class="verticalline"></div>
+		<div id="myblog">This Blog is for the students of IIT Kharagpur</div>
+	</div>
+	<div id="navigation">
+		<a href="index.php" style="display:block"><div id="home" class="textnav">HOME</div></a> 
+		<a href="post.php" style="display:block"><div id="articles" class="textnav">POSTS</div></a>
+		<a href="signup.php" style="display:block"><div id="signup" class="textnav">SIGN UP</div></a>
+	</div>
+	<div id="container2">
+		<div id="leftp">
+		<a style="display:block" href="http://justinbieber.com">
+		<div id="article1">
+			<div id="header"> Are IITs losing there charm? Find out seven main reasons...(Read more)</div>
+			<div id="image1"></div>
+			<div id="time1">06 August, 2015 @ 6:23 PM</div>
+			</div></a>
+			<a style="display:block" href="http://justinbieber.com">
+		<div id="article2">
+			<div id="header"> Stuff which societies in IIT Kharagpur do.</div><iframe style="margin-top:100px;margin-left:10%" width="80%" height="70%" src="https://www.youtube.com/embed/6FaqeflfEOE" frameborder="0" allowfullscreen></iframe>
+			<div id="time1" style:"color:blue">06 August, 2015 @ 8:03 PM</div>
+			</div></a>
+			<div id="comment">Comments : (User must be logged in )</div>
+			<form action="index.php" method="GET">
+				<input id="name" placeholder="Enter your name" name="name" disabled>
+				<textarea id="commentsection" placeholder="Enter your Comments here" name="text_ar" disabled></textarea>
+				<input id="submitbutton1" type="submit" value="Submit Comment" name="comment_submit" disabled>
+			</form>
+
+		<form action="comments.php" method="GET" name="comment">
+			<input id="submitbutton3" type="submit" value="Read Comments" name="read_comments">
+		</form>
+		</div>
+		<div id="vertical"></div>
+		<div id="rightportion">
+		<div id="log">
+		<form action="login.php" method="POST">
+		<p class="cont"> Username : </p>
+		<input type="text" placeholder="username" name="username" class="boxdesign" required >
+		<p class="cont"> Password : </p>
+		<input type="password" placeholder="password" name="password" class="boxdesign" required ><br><br>
+		<input id="submitbutton" type="submit"  value="Log in" name="submit">
+		</form>
+		</div>
+		<div id="morestory">
+		<h id="heading">More Stories (Outside Links)</h>
+		<br>
+		</div>
+		<div id="list">
+		<ul>
+			<li><a href="http://timesofindia.indiatimes.com/home/education/news/IIT-Kharagpur-sees
+			-growth-in-internship-offers/articleshow/48359213.cms" class="text">
+			IIT Khargapur sees growth in internship...(Read more)</a><br>
+			<li><a href="http://instiinfo.com/" class="text">Second year students of IIT Kharagpur makes new website
+			 to help freshers...(Visit website)</a><br><li>
+			<li><a href="http://indiaamericatoday.com/article/iit-kharagpur-law-school-signs-mou-gwu-vin-gupta-funds
+			-exchange" class="text">IIT Kharagpur Law School Signs MoU with GWU, Vin Gupta Funds Exchange...(Read more)</a><br>
+			<li><a href="http://www.iitkgp.ac.in/shownews.php?newsid=128" class="text">IIT Kharagpur Announces
+			 Distinguished Alumnus Awards 2015</a><br>
+			<li><a href="http://www.iitkgp.ac.in/shownews.php?newsid=124" class="text">IIT Kharagpur Students wins Go Green
+			 in the City 2015 Grand Finale International Award in Paris</a><br>
+			</u>
+		</div>
+	</div>
+
+</div>
+
+
+</body>
+</html>
